795db2afac544a170a95893c788e814cf39ca652
Erzyukov Alexey <erzyukov@gmail.com>
2025-05-28 08:02:41 +1000

    utilities extention methods

---
+++ Assets/Game/Scripts/Utilities/Extensions/Extensions.List.cs
@@ -0,0 +1,19 @@
+﻿namespace Game.Utilities
+{
+	using System.Collections.Generic;
+	using System;
+
+
+	public static class Extensions_List
+	{
+		public static void ForEach<T>(this IEnumerable<T> sequence, Action<int, T> action)
+		{
+			int i = 0;
+			foreach (T item in sequence)
+			{
+				action(i, item);
+				i++;
+			}
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Utilities/Extensions/Extensions.Vector.cs
@@ -0,0 +1,11 @@
+﻿namespace Game.Utilities
+{
+	using UnityEngine;
+
+
+	public static class Extensions_Vector
+	{
+		public static Vector2 WithX( this Vector2 v, float x )			{ v.x = x; return v; }
+		public static Vector2 WithY( this Vector2 v, float y )			{ v.y = y; return v; }	
+	}
+}
